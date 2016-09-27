2ef5911f52e49a9c2ed4f6661f3ccb16fb0c726f
dubr <dubr.cola@gmail.com>
2016-09-27 19:48:31 +0300

    wrappers

--- Grid/cfg.php
+++ Grid/cfg.php
@@ -3,8 +3,8 @@ use Floxim\Floxim\System\Fx as fx;
 
 return array(
     'actions' => array(
-        'two_columns' => array(
-            'name' => fx::alang('Two columns', 'widget_grid')
+        'show' => array(
+            'name' => 'Супер-колонки'
         )
     )
 );
