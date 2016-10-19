1f6c0cefbe387225e3109e509cf0f1534fd70d58
dubr <dubr.cola@gmail.com>
2016-10-19 15:22:24 +0300

    drop columns component

--- Column/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Floxim\Layout\Column;
-
-use Floxim\Floxim\System\Fx as fx;
-
-class Controller extends \Floxim\Main\Content\Controller 
-{
-
-}
--- Column/Finder.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Floxim\Layout\Column;
-
-use Floxim\Floxim\System\Fx as fx;
-
-class Finder extends \Floxim\Main\Content\Finder 
-{
-
-}
